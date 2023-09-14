a9a983ca7d63c6191d7822830b2298d0f821a14a
Luisa Tolentino <luisa.tolentino48@gmail.com>
2023-09-13 21:04:18 -0300

    atualização da atividade 5

--- atividade5/index.js
+++ atividade5/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.engine('html', mustacheExpress());
-app.set('view engine','html');
+app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.get("/", function(req, res){
-    res.render("index.html");
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 8080;
-app.listen(PORT, function(){
-    console.log("app rodando na porta", +PORT);
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/dados', (req, res) => {
+    const { nome, endereco, telefone, data } = req.body;
+    res.render('dados', { nome, endereco, telefone, data });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Atividade rodando na porta: ${PORT}`);
+});
